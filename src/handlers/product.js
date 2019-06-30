e8dfc869c39d95cdf74fb2e835ae88c57f2c272c
Henrique Cavalcante <henrique.cavalcante@yahoo.com.br>
2019-06-29 21:55:44 -0400

    chore: instalação e configuração do mongoose

--- src/handlers/product.js
+++ src/handlers/product.js
@@ -1,9 +1,20 @@
+const ProductModel = require("../models/product");
+
 const getAll = (request, h) => {
   return "Hello World!";
 };
 
-const save = () => {
-  return "Oi";
+const save = async (req, h) => {
+  // usando destructing
+  const { name, price } = req.payload;
+
+  const product = new ProductModel();
+  product.name = name;
+  product.price = price;
+
+  await product.save();
+
+  return h.response(product).code(201);
 };
 
 module.exports = {
---
+++ src/services/mongo.js
@@ -0,0 +1,4 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27018/ecommerce-api", {
+  useNewUrlParser: true
+});
